45e192fa8fcfb464d7acbe6df96d703a6c66b740
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2018-11-15 20:53:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,29 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "MA";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/alSayed")
-client.user.setStatus("dnd")
+const discord_token = "token";
+const prefix = "MA"
+client.login("NTA4NjI3MTMzNDcyNTcxNDE0.Ds85fQ.I-sj3YHT0wHBopKq1yz1GNwFeuE");
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '334062602554572800') return;
+
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
 });
-
-
-
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
