634e477ab7f718ebc0a0173276afefc669c89a36
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-06-01 15:46:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,13 @@
-const discord_token = "token";
-const prefix = "MA"
-client.login("NTA4NjI3MTMzNDcyNTcxNDE0.Ds85fQ.I-sj3YHT0wHBopKq1yz1GNwFeuE");
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '334062602554572800') return;
-
-if (message.content.startsWith(prefix + 'g')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + 'l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+const Discord = require("discord.js");
+const client = new Discord.Client();
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("576374909496983582");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**Welcome To Darkside. :sparkles: **`), 4000)        
 }
-
 });
+
+bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
